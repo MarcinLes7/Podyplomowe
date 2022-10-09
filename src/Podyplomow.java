c59e6075ab8ebd69e2f59b63d575c362d026ef9b
MarcinLes7 <lmartino@wp.pl>
2022-10-09 14:41:38 +0200

    Uaktualniony projekt o nowe przedmioty

--- src/Podyplomow.java
+++ src/Podyplomow.java
@@ -1,6 +1,8 @@
 public class Podyplomow {
     public static void main(String[] arg) {
-        int ocena = 4;
-        System.out.println("Ocena z zaliczenia to " + ocena + ".");
+        int ocenaGit = 4;
+        int ocenaJava = 4;
+        System.out.println("Ocena z zaliczenia GIT to " + ocenaGit + ".");
+        System.out.println("Ocena z zaliczenia Java to " + ocenaJava + ".");
     }
 }
